4aa5267c7485eaf100ed86f5d93b21d4086a2c57
charleshsu168 <hsu.charles@gmail.com>
2013-12-16 11:10:43 -0800

    working on 8.2.2

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,4 +9,16 @@ module UsersHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
+
+
+
+
+
