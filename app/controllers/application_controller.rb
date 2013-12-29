e194a899edae4d1c260696a7250467e170016af5
charleshsu168 <hsu.charles@gmail.com>
2013-12-28 22:11:52 -0800

    9.3.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
   # Force signout to prevent CSRF attacks
   def handle_unverified_request
     sign_out
